45e8205ed6c00ec813313ac7fb1e38a5ab1377d1
Stracke Dominik <stracke.dominik@gmail.com>
2024-01-10 20:25:02 +0100

    Update wezterm opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 return {
 	color_scheme = 'catppuccin-mocha',
 	font = wezterm.font 'JetBrains Mono',
+	window_background_opacity = 0.9,
 	font_size = 15,
 	hide_tab_bar_if_only_one_tab = true
 }
